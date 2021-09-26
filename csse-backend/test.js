573e6fcf194a1a397dd7837a88acdbdd2934cca6
M.L.Akila Liyanage <IT19120812@my.sliit.lk>
2021-09-26 17:47:23 +0530

    db configs all done

--- csse-backend/test.js
+++ csse-backend/test.js
@@ -26,6 +26,36 @@ router.get('/', async (req, res, _next) => {
     request.on('done', () =>{
         res.send(data)
     })
+
+});
+
+router.post('/', async (req, res, _next) => {
+
+    const ps = new sql.PreparedStatement();
+    ps.input('id',sql.VarChar(5))
+    ps.input('name',sql.VarChar(10))
+    ps.input('pass',sql.VarChar(10))
+    ps.input('email',sql.VarChar(20))
+    ps.input('type',sql.VarChar(20))
+
+    ps.prepare('insert into GeneralUser values(@id,@name,@pass,@email,@type);', err =>{
+        if(err) {
+            console.log(err);
+        }
+
+        ps.execute({id:'U0003',name:'Nethsara',pass:'nethsara',email:'nethsara@test.com',type:'Site Manager'},(err, result) =>{
+            if(err){
+                console.log(err);
+            }
+    
+            ps.unprepare(err =>{
+                if(err){
+                    console.log(err);
+                }
+            })
+        })
+    })
+
     
 });
 
