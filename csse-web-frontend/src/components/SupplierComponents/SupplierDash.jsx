cef6b63f4eb5aa5f185801255487015da53cd136
Nethsara Liyanage <tharusharb1012@gmail.com>
2021-10-05 10:03:07 +0530

    supplier new log

--- csse-web-frontend/src/components/SupplierComponents/SupplierDash.jsx
+++ csse-web-frontend/src/components/SupplierComponents/SupplierDash.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Avatar, Image, Dropdown, Button} from 'antd';
 import Logo1 from './../../assets/images/logo1.png';
 
 
@@ -28,6 +28,32 @@ class SupplierDash extends Component {
     }else if(this.state.selectedItem === '4'){
         // component = <MakeAppointments/>
     }
+    const menu = (
+      <Menu>
+        <Menu.Item 
+          // onClick={this.logout}
+        >
+          <a target="_blank" rel="noopener noreferrer" >
+              <Button block 
+                // onClick={this.logout}
+              >
+                  Profile
+              </Button>
+          </a>
+        </Menu.Item>
+        <Menu.Item 
+          // onClick={this.logout}
+        >
+          <a target="_blank" rel="noopener noreferrer" >
+              <Button block type="primary" danger 
+                // onClick={this.logout}
+              >
+                  LOG OUT
+              </Button>
+          </a>
+        </Menu.Item>
+      </Menu>
+  );
 
     return (
       <>
@@ -41,6 +67,13 @@ class SupplierDash extends Component {
                 <Menu.Item key="1" onClick={() => {this.setState({ selectedItem: '1'})}}>Requests</Menu.Item>
                 <Menu.Item key="2" onClick={() => {this.setState({ selectedItem: '2'})}}>Applied Requests</Menu.Item>
                 <Menu.Item key="3" onClick={() => {this.setState({ selectedItem: '3'})}}>Completed Requests</Menu.Item>
+                
+                <Menu.Item style={{marginLeft:'55%'}}>
+                  <Dropdown  overlay={menu} placement="bottomRight" arrow>
+                    <Avatar pre  src={<Image src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}  />
+                  </Dropdown>
+                </Menu.Item>
+                
               {/* {new Array(5).fill(null).map((_, index) => {
                 const key = index + 1;
                 return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
