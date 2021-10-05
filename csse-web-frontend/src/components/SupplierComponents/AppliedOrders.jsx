8bbc6df86d4a950ce6725566d499098053f1e5f3
Nethsara Liyanage <tharusharb1012@gmail.com>
2021-10-05 08:42:03 +0530

    supplier new log

--- csse-web-frontend/src/components/SupplierComponents/AppliedOrders.jsx
+++ csse-web-frontend/src/components/SupplierComponents/AppliedOrders.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Space, Input, Button, Card, Avatar, List, Tag, Badge} from "antd";
+import { Space, Input, Button, Card, Avatar, List, Tag, Badge, Modal} from "antd";
 import { Row, Col } from 'antd';
 import { Typography} from 'antd';
 import moment from "moment";
@@ -11,7 +11,10 @@ class AppliedOrders extends Component {
     super(props);
     this.state = {
       supplierId:'6',
-      myQuotas:[]
+      myQuotas:[],
+      selectedItem:{},
+      sendMore:0,
+      visible:false
     }
   }
 
@@ -22,11 +25,29 @@ class AppliedOrders extends Component {
     }).catch(err =>{
       console.log(err);
     })
-}
+  }
 
 componentDidMount(){
     this.fetchMyQuotas()
 }
+
+showModal = (item) => {
+  this.setState({
+    visible:true,
+    selectedItem:item,
+    sendMore:item.Item_No - item.quantity
+  })
+  console.log(item)
+};
+
+handleOk = () => {
+  this.setState({visible:false})
+};
+
+handleCancel = () => {
+  this.setState({visible:false})
+};
+
   render() {
     return (
       <div>
@@ -34,20 +55,25 @@ componentDidMount(){
       <Row>
         {this.state.myQuotas.map(item => {
           let color = 'yellow';
-          let status = 'Pending'
+          let status = 'Pending';
+          let disableStatus = ''
           if(item.status == "pending"){
             color = 'yellow';
-            status = 'Pending'
+            status = 'Pending';
+            disableStatus = 'disable'
           }else if(item.status == "panding"){
             color = 'yellow';
-            status = 'Pending'
+            status = 'Pending';
+            disableStatus = 'disable'
           }else if(item.status == "approved"){
             color = 'green';
             status = 'Approved'
           }else if(item.status == "rejected"){
             color = 'red';
-            status = 'Rejected'
+            status = 'Rejected';
+            disableStatus = 'disable'
           }
+          let sendMore =  item.Item_No - item.quantity;
           return(
             <Col span={12} style={{paddingRight:'20px'}}>
               <Badge.Ribbon text={status} color={color}>
@@ -70,13 +96,17 @@ componentDidMount(){
                         <Col><p>{item.request_price}/= per unit</p></Col>
                     </Row>
                     <Row style={{marginTop:5}}>
-                        <Col span={6}><Text strong>No of units you approved : </Text></Col>
-                        <Col><p><Tag color="#87d068">{item.quantity}</Tag></p></Col>
+                        <Col span={6}><Text strong>You have to send </Text></Col>
+                        <Col><p><Tag color="#87d068">{sendMore}</Tag> more Unit(s).</p></Col>
                     </Row>
                     <br />
                     <Text strong>Additional Informations you provided: </Text>
                     <p>{item.Additional_Description}</p>
-                    
+                    <Row style={{marginTop:10}}>
+                        <br />
+                          <Col span={16}></Col>
+                          <Col span={6}><Button span={6} disabled={disableStatus} type="primary" block onClick={() => this.showModal(item)}>Send a unit</Button></Col>
+                      </Row>
                 </Card>
               </Badge.Ribbon>
               <br /><br />
@@ -86,6 +116,18 @@ componentDidMount(){
         </Row>
         
       </Card>
+      <Modal
+        title="Send a unit"
+        visible={this.state.visible}
+        onOk={this.handleOk}
+        onCancel={this.handleCancel}
+      >
+        <Row style={{marginTop:5}}>
+          <Col span={6}><Text strong>You have to send </Text></Col>
+          <Col><p><Tag color="#87d068">{this.state.sendMore}</Tag> more Unit(s).</p></Col>
+        </Row>
+        
+      </Modal>
     </div>
     )
   }
